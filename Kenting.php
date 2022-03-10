f0018c0fffcfab48c9ee760ec1a7b28e685bc5d0
hsieh1093359 <100330536+hsieh1093359@users.noreply.github.com>
2022-03-11 00:07:49 +0800

    Add files via upload

---
+++ Kenting.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+<title>Kenting's trip</title>
+</head>
+<body bgcolor="#C8D8EE">
+<center><font size=60% color=#1BABF8 face ='微軟正黑體'><b>< 墾丁三日遊 ></b></font></center>
+<hr>
+<p style="color:#052840; font-size:25px;">
+1.行程內容：<p>
+<a href="https://www.google.com/search?q=%E5%A2%BE%E4%B8%81%E8%80%81%E8%A1%97" target = "new">
+    <img src="/墾丁老街.jpeg" border = '2' title = "墾丁大街" width="150" height="150"></a>
+    <a href="https://www.google.com/search?q=%E5%A2%BE%E4%B8%81%E5%9C%8B%E5%AE%B6%E5%85%AC%E5%9C%92" target = "new">
+    <img src="/墾丁國家公園.jpeg" border = '2' title = "墾丁國家公園" width="150" height="150"></a><br>    
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp1.墾丁大街
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp2.墾丁國家公園
+<p style="color:#052840; font-size:25px;">
+2.基本資料填寫：<p>
+<form>
+Name : <input type = "text"><br>
+Email : <input type = "email"><br>
+Tel&nbsp&nbsp&nbsp : <input type = "text"><br>
+
+Gender : 
+<input type="radio" value="1" name="1">男性
+<input type="radio" value="2" name="1">女性
+<input type="radio" value="3" name="1">其他<br>
+
+Food Preference : 
+<input type="checkbox" value="1" name="1">雞腿便當
+<input type="checkbox" value="2" name="2">燒肉便當
+<input type="checkbox" value="3" name="3">和牛便當<br>
+
+T-shirt Size : <select name = "Your Size">
+<option value = "Choice">請選擇</option>
+<option value = "1">S</option>
+<option value = "2">M</option>
+<option value = "3">L</option>
+<option value = "4">XL</option>
+</select><br>
+
+T-shirt Color : <input type = "text" value = "顏色"><br>
+Birthday : <input id="date" type="date"><br>
+Ticket : <input type = "number"><p>
+
+<input type="submit" name="enter" value="送出表單"><p>
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<衣服尺寸表>       
+</form>
+<table border = "1">
+  <tr>
+    <td><center>\</center></td>
+    <td><center>S</center></td>
+    <td><center>M</center></td>
+    <td><center>L</center></td>
+    <td><center>XL</center></td>
+  </tr>
+  <tr>
+    <td>衣長</td>
+    <td>68.5</td>
+    <td>70.0</td>
+    <td>71.5</td>
+    <td>73.0</td>
+  </tr>
+  <tr>
+    <td>衣寬</td>
+    <td>47.5</td>
+    <td>50.5</td>
+    <td>53.5</td>
+    <td>56.5</td>
+  </tr>
+  <tr>
+    <td>袖長</td>
+    <td>21.0</td>
+    <td>21.5</td>
+    <td>22.0</td>
+    <td>22.5</td>
+  </tr>
+</table><p> 
+<a href = "#top">回頁首</a>
+</body>
+</html>
